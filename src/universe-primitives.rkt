ec1939fa75f5b24859bd07b95eaaf1c0ed87a04f
leiDnedyA <aydendiel@gmail.com>
2023-08-07 09:35:25 -0400

    updated package implementation to match other universe primitives

--- src/universe-primitives.rkt
+++ src/universe-primitives.rkt
@@ -46,13 +46,15 @@
     ; [(and (struct? x) (prefab-struct-key x)) (for/and ((i (struct->vector x))) (sexp? i))]
     [else #false]))
 
-(define-values (make-package package? package-world package-message)
-  (let ()
-    (struct package (world message) #:transparent)
-    (define (make-package w m)
-    ;   (check-arg 'make-package (sexp? m) 'sexp "second" m)
-      (package w m))
-    (values make-package package? package-world package-message)))
+(struct u-package (world message))
+(define (make-package world message)
+  (u-package world message))
+(define (package? p)
+  (u-package? p))
+(define (package-world p)
+  (u-package-world p))
+(define (package-message p)
+  (u-package-message p))
 
 (struct u-bundle (state mails low-to-remove))
 (define (make-bundle state mails low-to-remove)
