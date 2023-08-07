acbc539473b0c8dc73a16967341cb12c8d181997
leiDnedyA <aydendiel@gmail.com>
2023-08-07 09:28:44 -0400

    added alert to client when server removes it's connection

--- main.rkt
+++ main.rkt
@@ -269,7 +269,7 @@
         (#js.conn.on #js"close" (λ (_) (
           ;; TODO: implement disconnect event
           #js*.console.log #js"conn closed")
-          ))
+          (#js*.alert #js"Client has been disconnected by the server or the connection has been lost.")))
         )))]
   ;; cb = (peer: Peer, conn: DataConnection) => void
   [add-peer-init-task
