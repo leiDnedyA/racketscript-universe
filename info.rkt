4aedc052f22ec661fe051857a45011260e40a79a
leiDnedyA <aydendiel@gmail.com>
2023-08-09 09:29:45 -0400

    updated pkg deps to pass github CI test

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "racketscript-universe")
-(define deps '("base"))
-(define build-deps '("racketscript/base" "scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps '("base" "racketscript/base" "racketscript-compiler"))
+(define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/racketscript-universe.scrbl" ())))
 (define pkg-desc "Implementation of htdp/universe in RacketScript with PeerJS")
 (define version "0.0")
